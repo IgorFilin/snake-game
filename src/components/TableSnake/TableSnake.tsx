5034ff54d21f5bc8200fbb346a8f53ae5d1cd1f0
Igor <filinigor@yandex.ru>
2022-12-10 22:34:31 +0300

    feat: create foot for snake in table

--- src/components/TableSnake/TableSnake.tsx
+++ src/components/TableSnake/TableSnake.tsx
@@ -1,26 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import s from './TableSnake.module.scss'
-import {getRandomGrid} from "../utils/getRandomGrid";
 import {CreateArrayGrid} from "../utils/CreateArrayGrid";
+
 
 export const TableSnake = () => {
 const [item,setItem] = useState({
-    row:10 as number,
-    column:10,
-    arrItems:[] as Array<{row:number,column:number}>
+    row: 10 as number,
+    column:10 as number,
+    arrItems:[] as Array<{row:number,column:number,isFood:boolean}>
 })
-
+    console.log(item.arrItems)
    useEffect(() => {
            const arrayGrid = CreateArrayGrid(item.row,item.column)
            setItem({...item,arrItems: [...arrayGrid]})
    },[])
 
-    getRandomGrid(item.row,item.column)
 
     return (
         <div className={s.mainContainer}>
                  <div className={s.tableSnake}>
-                     {item.arrItems.map((it)=> <div key={it.row + '' + it.column} className={s.tableItem}></div>)}
+                     {item.arrItems.map((it)=> <div key={it.row + '' + it.column} className={it.isFood ? s.tableFoot :s.tableItem}></div>)}
                  </div>
         </div>
     );
--- src/components/utils/CreateArrayGrid.ts
+++ src/components/utils/CreateArrayGrid.ts
@@ -1,8 +1,12 @@
+import {getRandomGrid} from "./getRandomGrid";
+
 export const CreateArrayGrid = (row:number,column:number) => {
     let arr = []
+    const foot = getRandomGrid(row,column)
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < column; j++) {
-            arr.push({row:i,column:j})
+            const isFood = foot.row === i && foot.col === j
+            arr.push({row:i,column:j,isFood})
         }
     }
     return arr
