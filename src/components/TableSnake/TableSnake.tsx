31a40121ca21b57f74c085e2229e395a5e347107
Igor <filinigor@yandex.ru>
2022-12-10 21:40:57 +0300

    feat: create tableSnake

--- src/components/TableSnake/TableSnake.tsx
+++ src/components/TableSnake/TableSnake.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './TableSnake.module.scss'
 
 export const TableSnake = () => {
+const [item,setItem] = useState({
+    row:10 as number,
+    column:10,
+    arrItems:[] as Array<{row:number,column:number}>
+})
+    console.log(item)
 
-    const arr = Array(100).fill(null,0,100)
+
+   useEffect(() => {
+       let arr = []
+           for (let i = 0; i < item.row; i++) {
+               for (let j = 0; j < item.column; j++) {
+                   arr.push({row:i,column:j})
+               }
+           }
+           setItem({...item,arrItems: [...arr]})
+   },[])
+
 
     return (
         <div className={s.mainContainer}>
                  <div className={s.tableSnake}>
-                     {arr.map(()=> <div className={s.tableItem}></div>)}
+                     {item.arrItems.map((it)=> <div key={it.row + '' + it.column} className={s.tableItem}></div>)}
                  </div>
         </div>
     );
