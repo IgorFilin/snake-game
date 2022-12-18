6ae044cf382dd32eb18eed625b71e7e849ebad5a
IgorFilin <filinigor@yandex.ru>
2022-12-18 16:01:57 +0300

    feat new logic snake

--- src/components/TableSnake/TableSnake.tsx
+++ src/components/TableSnake/TableSnake.tsx
@@ -1,255 +1,83 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import s from "./TableSnake.module.scss";
-import { createArrayGrid } from "../utils/createArrayGrid";
-import { getRandomGrid } from "../utils/getRandomGrid";
+import {createArrayGrid} from "../utils/createArrayGrid";
+import {getRandomGrid} from "../utils/getRandomGrid";
 
 export enum DIRECTION_TYPES {
-  RIGHT,
-  LEFT,
-  TOP,
-  BOTTOM,
+    RIGHT,
+    LEFT,
+    TOP,
+    BOTTOM,
 }
 
-export const TOTAL_COLUMNS = 10;
-export const TOTAL_ROWS = 10;
+const BOARD_SIZE = 10
+const DEFAULT_CELLS_VALUE = Array(BOARD_SIZE).fill(Array(BOARD_SIZE).fill(0))
+const AVAILABLE_MOVES = ['ArrowDown', 'ArrowUp', 'ArrowRight', 'ArrowLeft']
+const SPEED = 300
 
-export const MAX_COLUMN_INDEX = TOTAL_COLUMNS - 1;
-export const MAX_ROW_INDEX = TOTAL_ROWS - 1;
-
-export const MIN_COLUMN_INDEX = 0;
-export const MIN_ROW_INDEX = 0;
-
-export const INITIAL_COLUMN = 5;
-export const INITIAL_ROW = 5;
 
 export const TableSnake = () => {
-  const [arrItems, setArrItems] = useState(
-    createArrayGrid(TOTAL_ROWS, TOTAL_COLUMNS) as Array<{
-      row: number;
-      column: number;
-    }>
-  ); // Создаём массив с 100 ячейками
 
-  const [currentDirection, setCurrentDirection] = useState<DIRECTION_TYPES>(
-    DIRECTION_TYPES.BOTTOM
-  ); // направление движения   RIGHT,LEFT,TOP,BOTTOM
+    const [direction, setDirection] = useState(AVAILABLE_MOVES[0])
+    const [snake, setSnake] = useState([[1,1]])
+    console.log(snake)
+    const handleKeyDown = (event: KeyboardEvent) => {
 
-  const [currentHeadCoordinates, setCurrentHeadCoordinates] = useState({
-    row: INITIAL_ROW,
-    column: INITIAL_COLUMN,
-  }); // создаем координаты головы змейки
-
-  const [currentFoodCoordinates, setCurrentFoodCoordinates] = useState(
-    getRandomGrid(TOTAL_ROWS, TOTAL_COLUMNS) as { row: number; column: number }
-  ); // создаем координаты еды змеи
-
-  const [currentTailCoordinates, setCurrentTailCoordinates] = useState([
-    {
-      row: currentHeadCoordinates.row,
-      column: currentHeadCoordinates.column,
-    },{},{},{}
-  ] as Array<{ row: number; column: number }>); // создаем координаты хвоста змеи
-
-  const handleCurrentRowChange = (value: number) => {
-    setCurrentHeadCoordinates({
-      ...currentHeadCoordinates,
-      row: value,
-    });
-  }; // хендлер для смены положения головы змеи по рядам
-
-  const handleCurrentColumnChange = (value: number) => {
-    setCurrentHeadCoordinates({
-      ...currentHeadCoordinates,
-      column: value,
-    });
-  }; // хендлер для смены положения головы змеи по колонкам
-
-  useEffect(() => {
-    const id = setTimeout(() => {
-      switch (currentDirection) {
-        case DIRECTION_TYPES.LEFT:
-          if (currentHeadCoordinates.column !== MIN_COLUMN_INDEX) {
-            handleCurrentColumnChange(currentHeadCoordinates.column - 1);
-            setCurrentTailCoordinates(
-              currentTailCoordinates.map((el, i) => {
-                return {
-                  ...el,
-                  row: currentHeadCoordinates.row,
-                  column: currentHeadCoordinates.column + i - 1,
-                };
-              })
-            );
-            break;
-          }
-          break;
-        case DIRECTION_TYPES.RIGHT:
-          if (currentHeadCoordinates.column !== MAX_COLUMN_INDEX) {
-            handleCurrentColumnChange(currentHeadCoordinates.column + 1);
-            setCurrentTailCoordinates(
-              currentTailCoordinates.map((el, i) => {
-                return {
-                  ...el,
-                  row: currentHeadCoordinates.row,
-                  column: currentHeadCoordinates.column - i + 1,
-                };
-              })
-            );
-            break;
-          }
-          break;
-        case DIRECTION_TYPES.TOP:
-          if (currentHeadCoordinates.row !== MIN_ROW_INDEX) {
-            handleCurrentRowChange(currentHeadCoordinates.row - 1);
-            setCurrentTailCoordinates(
-              currentTailCoordinates.map((el, i,arr) => {
-                  return {
-                    ...el,
-                    row: currentHeadCoordinates.row - 1,
-                    column: currentHeadCoordinates.column - 1 - i,
-                  }
-              })
-            );
-            break;
-          }
-          break;
-        case DIRECTION_TYPES.BOTTOM:
-          if (currentHeadCoordinates.row !== MAX_ROW_INDEX) {
-            handleCurrentRowChange(currentHeadCoordinates.row + 1);
-            setCurrentTailCoordinates(
-              currentTailCoordinates.map((el, i) => {
-                return {
-                  ...el,
-                  row: currentHeadCoordinates.row - i + 1,
-                  column: currentHeadCoordinates.column,
-                };
-              })
-            );
-            break;
-          }
-          break;
-
-        default:
-          break;
-      }
-    }, 400);
-
-    return () => {
-      clearTimeout(id);
-    };
-  }, [currentDirection, currentHeadCoordinates]);
-  // let a = 1
-  useEffect(() => {
-    const handleChangeCurrentOnKey = (e: KeyboardEvent) => {
-      switch (e.keyCode) {
-        case 37:
-          setCurrentDirection(DIRECTION_TYPES.LEFT);
-          break;
-        case 38:
-          setCurrentDirection(DIRECTION_TYPES.TOP);
-          break;
-        case 39:
-        default:
-          setCurrentDirection(DIRECTION_TYPES.RIGHT);
-          break;
-        case 40:
-          setCurrentDirection(DIRECTION_TYPES.BOTTOM);
-          break;
-      }
-    };
-    document.addEventListener("keydown", (e) => handleChangeCurrentOnKey(e));
-
-    return () => {
-      document.removeEventListener("keydown", (e) =>
-        handleChangeCurrentOnKey(e)
-      );
-    };
-  }, []);
-
-  useEffect(() => {
-    if (
-      currentFoodCoordinates.row === currentHeadCoordinates.row &&
-      currentFoodCoordinates.column === currentHeadCoordinates.column
-    ) {
-      setCurrentFoodCoordinates(getRandomGrid(TOTAL_ROWS, TOTAL_COLUMNS));
-
-      if (currentDirection === DIRECTION_TYPES.BOTTOM) {
-        setCurrentTailCoordinates([
-          ...currentTailCoordinates,
-          {
-            row: currentTailCoordinates.slice(-1)[0].row - 1,
-            column: currentTailCoordinates.slice(-1)[0].column,
-          },
-        ]);
-      }
-      if (currentDirection === DIRECTION_TYPES.TOP) {
-        setCurrentTailCoordinates([
-          ...currentTailCoordinates,
-          {
-            row: currentTailCoordinates.slice(-1)[0].row + 1,
-            column: currentTailCoordinates.slice(-1)[0].column,
-          },
-        ]);
-      }
-      if (currentDirection === DIRECTION_TYPES.LEFT) {
-        setCurrentTailCoordinates([
-          ...currentTailCoordinates,
-          {
-            row: currentTailCoordinates.slice(-1)[0].row,
-            column: currentTailCoordinates.slice(-1)[0].column + 1,
-          },
-        ]);
-      }
-      if (currentDirection === DIRECTION_TYPES.RIGHT) {
-        setCurrentTailCoordinates([
-          ...currentTailCoordinates,
-          {
-            row: currentTailCoordinates.slice(-1)[0].row,
-            column: currentTailCoordinates.slice(-1)[0].column - 1,
-          },
-        ]);
-      }
+        const index = AVAILABLE_MOVES.indexOf(event.key)
+        if (index > -1) {
+            setDirection(AVAILABLE_MOVES[index])
+        }
     }
-  }, [currentFoodCoordinates, currentHeadCoordinates]); // хаваем
 
-  const getClassName = (isFood: boolean, isHead: boolean, isTail: boolean) => {
-    if (isFood) return s.tableFoot;
-    if (isHead) return s.headSnake;
-    if (isTail) return s.headSnake;
-    return s.tableItem;
-  };
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown)
+    }, [])
 
-  return (
-    <>
-      <div className={s.mainContainer}>
-        <div className={s.tableSnake}>
-          {arrItems.map((it) => {
-            const isHead =
-              it.column === currentHeadCoordinates.column &&
-              it.row === currentHeadCoordinates.row;
+    useEffect(()=>{
+      const id = setTimeout(() =>{
 
-            const isFood =
-              it.row === currentFoodCoordinates.row &&
-              it.column === currentFoodCoordinates.column;
+      let move:Array<number> = []
+          const newSnake = [...snake]
 
-            const isTail: boolean = currentTailCoordinates.reduce(
-              (acc, current) => {
-                if (it.row === current.row && it.column === current.column) {
-                  acc = true;
-                  return acc;
-                } else return acc;
-              },
-              false
-            );
+      switch (direction){
+          case AVAILABLE_MOVES[0]:{
+              move = [1,0]
+              break
+          }
+          case AVAILABLE_MOVES[1]:{
+              move = [-1,0]
+              break
+          }
+          case AVAILABLE_MOVES[2]:{
+              move = [0,1]
+              break
+          }
+          case AVAILABLE_MOVES[3]:{
+              move = [0,-1]
+              break
+          }
+      }
+          const head = [
+              newSnake[newSnake.length -1][0] + move[0],
+              newSnake[newSnake.length -1][1] + move[1]
+          ]
+          newSnake.push(head)
+          setSnake(newSnake.splice(1))
 
-            return (
-              <div
-                key={it.row + "-" + it.column}
-                className={getClassName(isFood, isHead, isTail)}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </>
-  );
-};
+      },SPEED)
+        return ()=>{
+          clearTimeout(id)
+        }
+    },[snake,direction])
+
+    return <div>
+        {DEFAULT_CELLS_VALUE.map((row, indexR) => {
+            return <div key={indexR} className={s.row}>
+                {row.map((cell:any, indexC:number) => {
+                    let type = snake.some(el => el[0] === indexR && el[1] === indexC)
+                    return <div key={indexC} className={type ? s.snake : s.cell}></div>
+                })}
+            </div>
+        })}
+    </div>
+}
